879b8f7d71183fcd3c5ba680b9d1f651647d501f
karllzy <li.zhenye@qq.com>
2022-06-13 00:51:34 +0800

    evaluation complete

--- README.md
+++ README.md
@@ -1,24 +1,25 @@
 # SCNet: A deep learning network framework for analyzing near-infrared spectroscopy using short-cut
+
 ## Pre-processing
 
 Since the method we proposed is a regression model, the classification dataset weat kernel is not used in this work.
 
-The other three dataset (corn, marzipan, soil) were preprocessed manually with Matlab and saved in the sub dictionary of `./preprocess` dir. The original  dataset of these three dataset were stored in the `./preprocess/dataset/`.
+The other three dataset (corn, marzipan, soil) were preprocessed manually with Matlab and saved in the sub dictionary of `./dataset` dir. The original  dataset of these three dataset were stored in the `./dataset/`. And the data are shared with google drive with this [link](https://drive.google.com/drive/folders/1RFREskNcI2sDv6p7lvLhxFRLUgVTwho6?usp=sharing)
 
-The mango dataset is not in Matlab .m file format, so we save them with the `process.py`. 
-Meanwhile, we drop the useless part and only save the data between  684 and 900 nm.
+The mango dataset is not in Matlab .m file format, so we save them with the `process.py`.
+Meanwhile, we drop the useless part and only save the data between  684 and 900 nm.
 
-All these datasets are available at this [link](https://drive.google.com/drive/folders/1RFREskNcI2sDv6p7lvLhxFRLUgVTwho6?usp=sharing)
+> The data set used in this study comprises a total of 11,691 NIR spectra (684–990 nm in 3 nm sampling with a total 103 variables) and DM measurements performed on 4675 mango fruit across 4 harvest seasons 2015, 2016, 2017 and 2018 [24].
 
-
-> The data set used in this study comprises a total of 11,691 NIR spectra (684–990 nm in 3 nm sampling with a total 103 variables) and DM measurements performed on 4675 mango fruit across 4 harvest seasons 2015, 2016, 2017 and 2018 [24]. 
-
-The detailed preprocessing progress can be found in [./preprocess.ipynb](./preprocess.ipynb)
+The detailed preprocessing progress can be found in [./preprocess.ipynb](./01_preprocess.ipynb)
 
 ## Network Training
 
-In order to show our network can prevent degration problem, we hold the experiment which contains the training loss curve of four models. The detailed information can be found in [model_training.ipynb](./model_training.ipynb).
+In order to show our network can prevent degration problem, we hold the experiment which contains the training loss curve of four models. The detailed information can be found in [model_training.ipynb](./02_model_training.ipynb).
+
+The training results were saved on the google drive, here is the [link](https://drive.google.com/drive/folders/1-p1SPg-6lt7i6NkgzUOf5GDhh0cDePsr?usp=sharing])
 
 ## Network evaluation
-After training our model on training set, we evaluate the models on testing dataset that spared before. The evaluation is done with [model_evaluation.ipynb](model_evaluating.ipynb).
+
+After training our model on training set, we evaluate the models on testing dataset that spared before. The evaluation is done with [model_evaluation.ipynb](03_model_evaluating.ipynb).
 
--- models.py
+++ models.py
@@ -1,3 +1,4 @@
+from tkinter import N
 import keras.callbacks
 import keras.layers as KL
 from keras import Model
@@ -9,7 +10,6 @@ class Plain5(object):
         self.model = None
         self.input_shape = input_shape
         if model_path is not None:
-            # TODO: loading from the file
             pass
         else:
             self.model = self.build_model()
@@ -135,9 +135,10 @@ class ShortCut5(object):
 
 
 class ShortCut11(object):
-    def __init__(self, model_path=None, input_shape=None):
+    def __init__(self, model_path=None, input_shape=None, network_parameter=200):
         self.model = None
         self.input_shape = input_shape
+        self.network_parameter = network_parameter
         if model_path is not None:
             # TODO: loading from the file
             pass
@@ -177,21 +178,25 @@ class ShortCut11(object):
         fx3 = KL.Activation('relu')(x)
         x = KL.Concatenate(axis=2)([x_raw, fx1, fx2, fx3])
 
-        x = KL.Dense(200, activation='relu', name='dense1')(x)
+        x = KL.Dense(self.network_parameter, activation='relu', name='dense1')(x)
         x = KL.Dense(1, activation='sigmoid', name='output')(x)
         model = Model(input_layer, x)
         return model
 
-    def fit(self, x, y, x_val, y_val, epoch, batch_size):
+    def fit(self, x, y, x_val, y_val, epoch, batch_size, save='checkpoints/shortcut11.hdf5', is_show=True):
         self.model.compile(loss='mse', optimizer=adam_v2.Adam(learning_rate=0.01 * (batch_size / 256)))
-        checkpoint = keras.callbacks.ModelCheckpoint(filepath='checkpoints/shortcut11.hdf5', monitor='val_loss',
-                                                     mode="min", save_best_only=True)
+        callbacks = []
+        checkpoint = keras.callbacks.ModelCheckpoint(filepath=save, monitor='val_loss',
+                                                 mode="min", save_best_only=True)
+        callbacks.append(checkpoint)
         early_stop = keras.callbacks.EarlyStopping(monitor='val_loss', min_delta=1e-6,
                                                    patience=200, verbose=0, mode='auto')
         lr_decay = keras.callbacks.ReduceLROnPlateau(monitor='val_loss', factor=0.5,
                                                      patience=25, min_delta=1e-6)
-        callbacks = [checkpoint, early_stop, lr_decay]
-        history = self.model.fit(x, y, validation_data=(x_val, y_val), epochs=epoch, verbose=1,
+        callbacks.append(early_stop)
+        callbacks.append(lr_decay)
+        verbose_num = 1 if is_show else 0
+        history = self.model.fit(x, y, validation_data=(x_val, y_val), epochs=epoch, verbose=verbose_num,
                                  callbacks=callbacks, batch_size=batch_size)
         return history
 
@@ -257,8 +262,56 @@ class Plain11(object):
         return history
 
 
+class SimpleCNN(object):
+    def __init__(self, model_path=None, input_shape=None):
+        self.model = None
+        self.input_shape = input_shape
+        if model_path is not None:
+            pass
+        else:
+            self.model = self.build_model()
+
+    def build_model(self):
+        input_layer = KL.Input(self.input_shape, name='input')
+        x = KL.Conv1D(8, 7, padding='same', strides=3, name='Conv1')(input_layer)
+        x = KL.BatchNormalization()(x)
+        x = KL.Activation('relu')(x)
+
+        x = KL.Conv1D(8, 3, padding='same', strides=3, name='Conv2')(x)
+        x = KL.BatchNormalization()(x)
+        x = KL.Activation('relu')(x)
+
+        x = KL.Conv1D(8, 3, padding='same', strides=1, name='Conv3')(x)
+        x = KL.BatchNormalization()(x)
+        x = KL.Activation('relu')(x)
+
+        x = KL.Conv1D(8, 9, padding='same', strides=3, name='Conv4')(x)
+        x = KL.BatchNormalization()(x)
+        x = KL.Activation('relu')(x)
+
+        x = KL.Dense(20, activation='relu', name='dense')(x)
+        x = KL.Dense(1, activation='sigmoid', name='output')(x)
+        model = Model(input_layer, x)
+        return model
+
+    def fit(self, x, y, x_val, y_val, epoch, batch_size):
+        self.model.compile(loss='mse', optimizer=adam_v2.Adam(learning_rate=0.01 * (batch_size / 256)))
+        checkpoint = keras.callbacks.ModelCheckpoint(filepath='checkpoints/plain5.hdf5', monitor='val_loss',
+                                                     mode="min", save_best_only=True)
+        early_stop = keras.callbacks.EarlyStopping(monitor='val_loss', min_delta=0,
+                                                   patience=1000, verbose=0, mode='auto')
+        lr_decay = keras.callbacks.ReduceLROnPlateau(monitor='val_loss', factor=0.5, patience=25, min_delta=1e-6)
+        callbacks = [checkpoint, early_stop, lr_decay]
+        history = self.model.fit(x, y, validation_data=(x_val, y_val), epochs=epoch, verbose=1,
+                                 callbacks=callbacks, batch_size=batch_size)
+        return history
+
+
+
 if __name__ == '__main__':
     # plain5 = Plain5(model_path=None, input_shape=(1, 102))
     # plain11 = Plain11(model_path=None, input_shape=(1, 102))
     residual5 = Residual5(model_path=None, input_shape=(1, 102))
     short5 = ShortCut5(model_path=None, input_shape=(1, 102))
+    sample = SimpleCNN(model_path=None, input_shape=(1, 102))
+
